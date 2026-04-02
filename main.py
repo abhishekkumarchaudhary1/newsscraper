2b00c6192fb46d29788ea75d7b92a994984b3672
abhishekkumarchaudhary1 <abhishek.chaudhary33@gmail.com>
2026-04-03 03:09:01 +0530

    Enabling cors for all origins

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from supabase import create_client
 import os
 from dotenv import load_dotenv
@@ -8,6 +9,14 @@ from scraper import run_scraper
 load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
